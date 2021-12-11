febeb4937ec0a1c9358f060b52fb16240b3df47e
amir-zrn <amir.zareiyan01@gmail.com>
2021-12-11 18:04:20 +0330

    dark mode finished, grid changes🐧

--- src/components/darkSwitch.js
+++ src/components/darkSwitch.js
@@ -1,28 +1,52 @@
 import React from "react"
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
-import { faCoffee } from "@fortawesome/free-solid-svg-icons"
+import moon from "../../content/assets/moon.svg"
+import sun from "../../content/assets/sun.svg"
 
 const DarkSwitch = () => {
-  const [dark, setDark] = React.useState(() => {
-    const storageDarkValue = window.localStorage.getItem("darkMode")
+  let clickedClass = "clicked"
+  const body = document.body
+  const lightTheme = "light"
+  const darkTheme = "dark"
+  let theme
+  let imgSrc = []
 
-    return storageDarkValue !== null ? storageDarkValue : false
-  })
+  const [darkImg, setDarkImg] = React.useState(moon)
 
-  React.useEffect(() => {
-    if (dark) {
-      document.body.classList.toggle("dark")
+  if (localStorage) {
+    theme = localStorage.getItem("theme")
+  }
+
+  if (theme === lightTheme || theme === darkTheme) {
+    body.classList.add(theme)
+  } else {
+    body.classList.add(lightTheme)
+  }
+
+  const switchTheme = e => {
+    if (theme === darkTheme) {
+      body.classList.replace(darkTheme, lightTheme)
+      e.target.classList.remove(clickedClass)
+      localStorage.setItem("theme", "light")
+      theme = lightTheme
+      setDarkImg({ sun })
+      imgSrc = darkImg
     } else {
-      document.body.classList.toggle("light")
+      body.classList.replace(lightTheme, darkTheme)
+      e.target.classList.add(clickedClass)
+      localStorage.setItem("theme", "dark")
+      theme = darkTheme
+      setDarkImg({ moon })
+      imgSrc = darkImg
     }
-    window.localStorage.setItem("darkMode", dark)
-  }, ["darkMode", dark])
+    console.log(imgSrc[Object.keys(imgSrc)[0]])
+  }
 
   return (
-    <button onClick={() => setDark(!dark)}>
-      <FontAwesomeIcon icon={faCoffee} />
-      are
-    </button>
+    <button
+      className={theme === "dark" ? clickedClass : ""}
+      id="darkMode"
+      onClick={e => switchTheme(e)}
+    ></button>
   )
 }
 
